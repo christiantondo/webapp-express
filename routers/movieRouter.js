78151acd8050fb0795417dd5e3a77d4fc3c331a8
Christian Tondo <christiantondo1@gmail.com>
2026-03-30 23:03:50 +0200

    added storeReview

--- routers/movieRouter.js
+++ routers/movieRouter.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const movieController = require('../controllers/movieController');
 
-router.get('/', movieController.index)
-router.get('/:id', movieController.show)
+router.get('/', movieController.index);
+router.get('/:id', movieController.show);
+router.post('/:id/reviews', movieController.storeReview);
 
 module.exports = router;
