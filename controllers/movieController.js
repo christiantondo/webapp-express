5fc520eaf4daa796e0a4e609d6a9ea18b8a315cc
Christian Tondo <christiantondo1@gmail.com>
2026-03-30 20:57:19 +0200

    average review added

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,66 +1,67 @@
 const db = require('../data/db');
 
 const index = (req, res) => {
-	const sqlQuery = 'SELECT * FROM movies';
-	db.query(sqlQuery, (err, results) => {
-		if (err) {
-			console.error('DB QUERY FAILED', err);
-			return res.status(500).json({
-				error: 'Database error',
-				message: err.message
-			})
-		}
+    const sqlQuery = 'SELECT * FROM movies';
+    db.query(sqlQuery, (err, results) => {
+        if (err) {
+            console.error('DB QUERY FAILED', err);
+            return res.status(500).json({
+                error: 'Database error',
+                message: err.message
+            })
+        }
 
-		res.json(results)
-	});
+        res.json(results)
+    });
 }
 
 const show = (req, res) => {
-	const id = req.params.id;
+    const id = req.params.id;
 
-	const sqlQueryMovies = 'SELECT * FROM movies WHERE id = ?';
-	const sqlQueryReviews = 'SELECT * FROM reviews WHERE movie_id = ?';
+    const sqlQueryMovies = 'SELECT movies.*, ROUND(AVG(reviews.vote)) AS average_review FROM movies JOIN reviews ON reviews.movie_id = movies.id WHERE movies.id = ?';
+    const sqlQueryReviews = 'SELECT * FROM reviews WHERE movie_id = ?';
 
-	db.query(sqlQueryMovies, [id], (err, movies) => {
+    db.query(sqlQueryMovies, [id], (err, movies) => {
 
-		if (err) {
-			console.error('DB QUERY FAILED', err);
-			return res.status(500).json({
-				error: 'Database error',
-				message: err.message
-			})
-		}
+        if (err) {
+            console.error('DB QUERY FAILED', err);
+            return res.status(500).json({
+                error: 'Database error',
+                message: err.message
+            })
+        }
 
-		if (movies.length === 0 || movies[0].id === null) {
-			return res.status(404).json({
-				error: 'Not found',
-				message: 'Could not find requested movie'
-			});
-		}
+        if (movies.length === 0 || movies[0].id === null) {
+            return res.status(404).json({
+                error: 'Not found',
+                message: 'Could not find requested movie'
+            });
+        }
 
-		const movie = movies[0];
-		console.log('initial movie', movie);
+        const movie = movies[0];
+        movie.average_review = Number(movie.average_review);
+        console.log('initial movie', movie);
 
-		db.query(sqlQueryReviews, [id], (err, reviews) => {
+        db.query(sqlQueryReviews, [id], (err, reviews) => {
 
-			if (err) {
-				console.error('DB QUERY FAILED', err);
-				return res.status(500).json({
-					error: 'Database error',
-					message: err.message
-				});
-			}
+            if (err) {
+                console.error('DB QUERY FAILED', err);
+                return res.status(500).json({
+                    error: 'Database error',
+                    message: err.message
+                });
+            }
 
-			movie.reviews = reviews;
+            movie.reviews = reviews;
 
-			console.log('reviews', reviews);
-			console.log('movie with reviews', movie);
+            console.log('reviews', reviews);
+            console.log('movie with reviews', movie);
 
-			res.json(movie);
-		});
+            res.json(movie);
+        });
 
 
-	});
+    });
 }
 
 module.exports = { index, show };
